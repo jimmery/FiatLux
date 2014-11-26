f8fb1816002e02ac5373d2d67b70ea4eaca66824
jimmery <jiangxuanzhi@gmail.com>
2014-11-26 10:55:16 -0800

    First Working Servo Code
    This is our first test for the servo, we can come back to this code if
    needed, but the real work is to begin here.

--- FiatLux_Controller/FiatLux_Controller.ino
+++ FiatLux_Controller/FiatLux_Controller.ino
@@ -1,1 +1,39 @@
+#include <Servo.h>
 
+#define ServoPin 9
+#define LEDPin 13
+Servo myServo;
+
+void setup()
+{
+  pinMode(ServoPin, OUTPUT);
+  pinMode(LEDPin, OUTPUT);
+  
+  myServo.attach(ServoPin);
+//  myServo.write(0);
+}
+
+
+void loop()
+{
+//  //analogWrite(myPWM, 100);
+//  myServo.write(180);
+//  digitalWrite(LEDPin, HIGH);
+//  delay(1000);
+//  //analogWrite(myPWM, 0);
+//  myServo.write(0);
+//  digitalWrite(LEDPin, LOW);
+//  delay(1000);
+
+  for ( int i = 0; i < 180; i++ )
+  {
+    myServo.write(i);
+    delay(15);
+  }
+  
+  for ( int i = 180; i >= 0; i-- )
+  {
+    myServo.write(i);
+    delay(15);
+  }  
+}
