6eee72328ff9b78d798ac9ac96e739abe0aefb06
jimmery <jiangxuanzhi@gmail.com>
2014-12-04 23:51:13 -0800

    Modularized the code
    Separated the Controller, which will contain just the large overview and
    the detailed code, which will be in their respective files. In this, we
    should have implemented the theta search completely, but it has not
    completely been tested.

--- FiatLux_Controller/FiatLux_Controller.ino
+++ FiatLux_Controller/FiatLux_Controller.ino
@@ -15,7 +15,7 @@ const int HIGH_RF_THRESHOLD = 150;
 unsigned int MIN_THETA = 0; 
 unsigned int data = 0; //data input from RF 
 unsigned int firstTheta = 0;
-unsigned int secondTheta = 0;
+unsigned int secondTheta = MAX_THETA;
 
 
 Servo botServo;
@@ -34,74 +34,12 @@ void setup()
 
 void loop()
 {
-bool leaveLoop1 = false; //use this to get out of loops when angles are found
-bool leaveLoop2 = false;
-
-//Going up on scan grid 
-for(int phi = 0; phi < MAX_PHI;){ 
-  for ( int theta = MIN_THETA; theta < MAX_THETA; theta++ )
-  {
-    digitalWrite(signalLEDPin, HIGH); //turn on the LED to start getting info from the boat
-    data = analogRead(receiver);  //get signal from RF sent from boat 
-    botServo.write(theta);
-    if(data > HIGH_RF_THRESHOLD){ //check to see if boat is finding a high
-      firstTheta = theta;
-      leaveLoop1 = true;
-    }
-    else if(data < LOW_RF_THRESHOLD){
-      secondTheta = theta;
-      leaveLoop2 = true;
-    }    
-   
-    delay(15); //change this for speed
-  }
-  
-   if(leaveLoop1 && leaveLoop2){ //both theta angles have been found, leave the loop
-       break;
-   }  
-  
-  phi+=2; //Potential to change phi direction movement
-  topServo.write(phi); 
-  
-  for ( int theta = MAX_THETA; theta >= MIN_THETA; theta-- )
-  {
-    botServo.write(theta);
-    delay(15);
-   
-  }  
-  
-  phi+=2;
-  topServo.write(phi); //Change this increment for phi direction 
-}  
 
 
-if(leaveLoop1 && leaveLoop2){  //if theta values have been found, use these as the new limits
-  MIN_THETA = firstTheta;
-  MAX_THETA = secondTheta;
-}
+  while (!thetaSearch());
 
 
 
-//Going down on scan grid
-for(int phi = MAX_PHI; phi >0;){ //Change upper limit for phi
-  for ( int theta = MIN_THETA; theta < MAX_THETA; theta++ )
-  {
-    botServo.write(theta);
-    delay(15); //change this for speed
-  }
-  
-  phi-=2;
-  topServo.write(phi); //Change this increment for phi direction 
-  
-  for ( int theta = MAX_THETA; theta >= MIN_THETA; theta-- )
-  {
-    botServo.write(theta);
-    delay(15);
-  }  
-  
-  phi-=2;
-  topServo.write(phi); //Change this increment for phi direction 
-}  
  
  
 }
--- FiatLux_Controller/Lighthouse.ino
+++ FiatLux_Controller/Lighthouse.ino
@@ -14,6 +14,111 @@
 // boat will reset essentially to prepare for laser input. 
 
 // laser search. 
+boolean thetaSearch()
+{
+  bool minAngFound = false; //use this to get out of loops when angles are found
+  bool maxAngFound = false;
+  
+  //Going up on scan grid 
+  for(int phi = 0; phi < MAX_PHI;){ 
+    for ( int theta = firstTheta; theta < secondTheta; theta++ )
+    {
+      digitalWrite(signalLEDPin, HIGH); //turn on the LED to start getting info from the boat
+      botServo.write(theta);
+      data = analogRead(receiver);  //get signal from RF sent from boat 
+      if(data > HIGH_RF_THRESHOLD && !minAngFound){ //check to see if boat is finding a high
+        firstTheta = theta;
+        minAngFound = true;
+      }
+      else if(data < LOW_RF_THRESHOLD && minAngFound && !maxAngFound ){
+        secondTheta = theta;
+        maxAngFound = true;
+        return true;
+      }
+      delay(15); //change this for speed
+    }
+    
+    if ( minAngFound )
+    {
+      return true;
+    }
+  
+    phi+=2; //Potential to change phi direction movement
+    topServo.write(phi); 
+  
+    for ( int theta = MAX_THETA; theta >= MIN_THETA; theta-- )
+    {
+      botServo.write(theta);
+      data = analogRead(receiver);  //get signal from RF sent from boat 
+      if(data > HIGH_RF_THRESHOLD && !maxAngFound){ //check to see if boat is finding a high
+        secondTheta = theta;
+        maxAngFound = true;
+      }
+      else if(data < LOW_RF_THRESHOLD && maxAngFound && !minAngFound ){
+        firstTheta = theta;
+        minAngFound = true;
+        return true;
+      }
+      delay(15);
+    }
+    
+    if ( maxAngFound )
+      return true;
+  
+    phi+=2;
+    topServo.write(phi); //Change this increment for phi direction 
+  }  
+
+  //Going down on scan grid
+  for(int phi = MAX_PHI; phi >0;){ //Change upper limit for phi
+    for ( int theta = MIN_THETA; theta < MAX_THETA; theta++ )
+    {
+      botServo.write(theta);
+      data = analogRead(receiver);  //get signal from RF sent from boat 
+      if(data > HIGH_RF_THRESHOLD && !minAngFound){ //check to see if boat is finding a high
+        firstTheta = theta;
+        minAngFound = true;
+      }
+      else if(data < LOW_RF_THRESHOLD && minAngFound && !maxAngFound ){
+        secondTheta = theta;
+        maxAngFound = true;
+        return true;
+      }
+      delay(15); //change this for speed
+    }
+    
+    if ( minAngFound )
+    {
+      return true;
+    }
+  
+    phi-=2;
+    topServo.write(phi); //Change this increment for phi direction 
+  
+    for ( int theta = MAX_THETA; theta >= MIN_THETA; theta-- )
+    {
+      botServo.write(theta);
+      data = analogRead(receiver);  //get signal from RF sent from boat 
+      if(data > HIGH_RF_THRESHOLD && !maxAngFound){ //check to see if boat is finding a high
+        secondTheta = theta;
+        maxAngFound = true;
+      }
+      else if(data < LOW_RF_THRESHOLD && maxAngFound && !minAngFound ){
+        firstTheta = theta;
+        minAngFound = true;
+        return true;
+      }
+      delay(15);
+    }
+    
+    if ( maxAngFound )
+      return true;  
+  
+    phi-=2;
+    topServo.write(phi); //Change this increment for phi direction 
+  }
+  return false;
+}
 
 // search will be done with diagonal thingies. 
 // choose a somewhat small offset for theta and phi (if applicable) and do a search. 
