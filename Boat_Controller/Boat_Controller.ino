6677a91d35f6c1dfb013f030b894f65815d63e20
jdhurwitz <jdhurwitz@ucla.edu>
2014-12-10 16:06:37 -0800

    Boat pseudocode and cleanups
    Cleaned up some things, removed redundancy, and added some new
    pseudocode.

--- Boat_Controller/Boat_Controller.ino
+++ Boat_Controller/Boat_Controller.ino
@@ -1,20 +1,7 @@
-// setup. 
-// take ambient light and store it. 
-
-
-// continue taking in light values. 
-// subtract ambient light. 
-// experiment with one of these two methods: 
-// 1. we can start transmitting RF when detecting a significant change in delta. 
-   // we will have to define significant. 
-// 2. we do analysis on first two detections to determine an approximate HIGH value and range. 
-   // transmit when next HIGH value is detected (within a range). 
-
-
-// we can consider doing a time delay here, depending on what we decide to do with the lighthouse. 
-
-
-// when receiving a much higher value (for the laser), transmit a signal to stop the lighthouse. 
+//Takes in ambient light values up to upper limit specification and averages them.
+//If greater than baseline, this means LED has hit for the first theta.
+//After this state, this means that the LED has started to fall off and this is the second theta. Trasmit a low.
+//The code now knows that the laser will be searching for it. When the light value is greater than laser baseline, transmit a constant high.
 
 #define lightReceiver A0
 #define laserPin 2
@@ -22,12 +9,11 @@
 #define RFoutput 4
 
 
-const int SAMPING_NUM = 50;
-const int BASELINE_OFFSET = 10; //change based on need
-const int LASER_BASELINE = 550; //change value for laser code
-const int NOISE_CONSTANT = 0;
-int lightData = 0;
-int lightBaseline = 0; //this will be set to the averaged ambient value
+const int SAMPING_NUM = 50;     //Number of ambient values to be averaged.
+const int BASELINE_OFFSET = 10; //Compensation constant for LED sensor.
+const int LASER_BASELINE = 550; //Baseline laser intensity value.
+int lightData = 0;              //Variable used for light values.
+int lightBaseline = 0;          //Variable set to value of ambient light.
 
 boolean executeLaserCode = false;
 boolean executeLEDCode = true;
@@ -43,7 +29,7 @@ void setup(){
  digitalWrite(laserPin, HIGH);
  digitalWrite(RFoutput, LOW);
  
- for(int i = 0; i< SAMPING_NUM; i++){  //if 50 values is too large we can decrease this so as to not have neg int vals 
+ for(int i = 0; i< SAMPING_NUM; i++){        //if 50 values is too large we can decrease this so as to not have neg int vals 
     lightData = analogRead(lightReceiver);
     lightBaseline += lightData;
  }
@@ -56,33 +42,28 @@ void setup(){
 void loop(){
   delay(100);
   lightData = analogRead(lightReceiver);
-  //Serial.println(lightData);
 
   //Laser code
   if(transmitHigh){
      digitalWrite(RFoutput, HIGH); 
-      Serial.println("transmit HIGH");
+     Serial.println("transmit HIGH");
 
   }else{
       if(executeLaserCode){
         if(lightData > LASER_BASELINE){
             digitalWrite(RFoutput, HIGH); 
             Serial.println("laser HIGH");
-
             transmitHigh = true;
           }else{
             digitalWrite(RFoutput, LOW); 
-             Serial.println("laser LOW");
-
+            Serial.println("laser LOW");
           }
       }else if(executeLEDCode){ 
-        
-  //Once baseline is found we want to check for the LED on the boat and then signal HIGH
+      //Once baseline is found we want to check for the LED on the boat and then signal HIGH.
       lightData = analogRead(lightReceiver);
       if(lightData > lightBaseline) { //this means we are getting data from non-ambient light
         digitalWrite(RFoutput, HIGH); //transmit high to signal lighthouse that first theta has been found
-         Serial.println("led HIGH");
-
+        Serial.println("led HIGH");
         LEDHigh = true;
       }      
       else{
@@ -93,11 +74,8 @@ void loop(){
         } 
          digitalWrite(RFoutput, LOW);  
          Serial.println("led LOW");
-
       }
      }
     }
-
- 
 }
 
